f206aa35d99f022aab31ddb5ae38fc281ef69a6f
Gleb 1.1.1 <gleb_rezvov@mail.ru>
2026-07-04 13:44:26 +0700

    Remove generated files

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,31 @@
+package model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Qualifier("dog")
+    @Autowired
+    private Animal animal;
+
+    @Qualifier("timer")
+    @Autowired
+    private Timer timer;
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(timer.getTime());
+        System.out.println("________________________");
+    }
+
+
+}
